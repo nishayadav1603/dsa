80f3240cc2833c3d0aa237ee3fb3c5374e312a6c
Nisha Yadav <nishayadav30465@gmail.com>
2023-02-14 13:55:19 +0530

    done

--- Arrays/uniqueelement.cpp
+++ Arrays/uniqueelement.cpp
@@ -12,14 +12,12 @@ int findunique(vector<int>arr){
 
 int main(){
     //find unique element
-    int n;
-    cout << "Enter the size of arr: " << endl;
-    cin >> n;
-    vector<int> arr(n);
-    cout << "Enter the elements: " << endl;
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
-    }
+    vector<int>arr;
+    arr.push_back(1);
+    arr.push_back(3);
+    arr.push_back(2);
+    arr.push_back(2);
+    arr.push_back(3);
     int ans = findunique(arr);
     cout << "Unique elements:  " << ans << endl;
     return 0;
