9c4c173172b1265301534b50d7b6284eec469866
Nisha Yadav <nishayadav30465@gmail.com>
2023-02-19 00:05:40 +0530

    done

--- Arrays/firstrepeating.cpp
+++ Arrays/firstrepeating.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 
 int main(){
     int arr[] = {10,5,3,4,3,5,6};
     int n = 7;
+    // for(int i=0;i<n;i++){
+    //     for(int j=i+1;j<n;j++){
+    //         if(arr[i] == arr[j]){
+    //             cout << arr[i] << " ";
+    //         }
+    //     }
+    // }
+
+    unordered_map<int,int> hash;
     for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i] == arr[j]){
-                cout << arr[i] << " ";
-            }
+        hash[arr[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(hash[arr[i]] > 1){
+            return i+1;
         }
     }
-    return 0;
+    return -1;
 }
