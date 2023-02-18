1d2d9b625666cb503e738b432906490d9a3b8c45
Nisha Yadav <nishayadav30465@gmail.com>
2023-02-18 23:33:24 +0530

    done

--- Arrays/duplicates.cpp
+++ Arrays/duplicates.cpp
@@ -1,13 +1,44 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int arr[] = {1,2,3,6,3,6,1};
-    int n  = 7;
-    int ans = 0;
-    for(int i=0;i<n;i++){
-        ans = ans^arr[i];
+int findDup(int arr[],int n){
+    sort(arr,arr+n);
+    for(int i=0;i<n-1;i++){
+        if(arr[i] == arr[i+1]){
+            return arr[i];
+        }
     }
+    return -1;
+}
+
+int findDup1(int arr[],int n){
+    int ans = -1;
+    for(int i=0;i<n;i++){
+        int index = abs(arr[i]);
+        if(arr[index]<0){
+            return index;
+        }
+        arr[index] *= -1;
+    }  
+    return ans;
+}
+
+int findDup2(int arr[],int n){
+    while(arr[0] != arr[arr[0]]){
+        swap(arr[0],arr[arr[0]]);
+    }
+    return arr[0];
+}
+
+int main(){
+    int arr[] = {1,3,4,2,2};
+    int n  = 5;
+
+    //int ans = findDup(arr,n);
+
+    //int ans = findDup1(arr,n);
+
+    int ans = findDup2(arr,n);
     cout << ans << " ";
     return 0;
 }
