f991e8bbad75f3c9d305132f52d10b3e33bb57de
Nisha Yadav <nishayadav30465@gmail.com>
2023-02-20 11:44:35 +0530

    square root of a number

--- Binary Search/sqrt.cpp
+++ Binary Search/sqrt.cpp
@@ -4,26 +4,28 @@ using namespace std;
 int mySqrt(int x) {
         int s = 0;
         int e = x;
+        int target = x;
         int mid = s + (e-s)/2;
         int ans = -1;
         while(s <= e){
-            if(mid*mid == x){
+            if(mid*mid == target){
                 return mid;
             }
-            else if(mid*mid < x){
+            else if(mid*mid < target){
                 ans = mid;
                 s = mid + 1;
             }
             else{
                 e = mid - 1;
             }
+            mid = s + (e-s)/2;
         }
         return ans;
         
 }
 
 int main(){
-    int n = 8;
+    int n = 20;
     int ans = mySqrt(n);
     cout << ans << endl;
     return 0;
