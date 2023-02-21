d0a5d9cf7da2e2f942f1cae2b11b9bf0116db7e1
Nisha Yadav <nishayadav30465@gmail.com>
2023-02-21 13:10:48 +0530

    done

--- Arrays/Arrays/firstrepeating.cpp
+++ Arrays/Arrays/firstrepeating.cpp
@@ -5,11 +5,13 @@ int main(){
     int arr[] = {10,5,3,4,3,5,6};
     int n = 7;
     for(int i=0;i<n;i++){
+        bool isrepeated = false;
         for(int j=i+1;j<n;j++){
             if(arr[i] == arr[j]){
-                cout << arr[i] << " ";
+                isrepeated = true;
+                cout << i+1 << " ";
             }
         }
     }
-    return 0;
+    return -1;
 }
